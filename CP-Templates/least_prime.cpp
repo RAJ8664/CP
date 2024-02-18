37cb39ebc3146b1312c70af6a8d434e0acd6c15e
UNKNOWN <119309033+RAJ8664@users.noreply.github.com>
2024-02-19 00:13:07 +0530

    Add files via upload

---
+++ CP-Templates/least_prime.cpp
@@ -0,0 +1,161 @@
+/*Author : RAJ ROY*/
+/*scholar id : 2212002*/
+
+#include <bits/stdc++.h>
+using namespace std;
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <algorithm>
+#include <iostream>
+#include <algorithm>
+#define endl "\n"
+#define ll long long
+#define EPS 1e-9
+
+/********************************************** Debug Code **************************************************/
+void __print(int x) { cerr << x; }
+void __print(long x) { cerr << x; }
+void __print(long long x) { cerr << x; }
+void __print(unsigned x) { cerr << x; }
+void __print(unsigned long x) { cerr << x; }
+void __print(unsigned long long x) { cerr << x; }
+void __print(float x) { cerr << x; }
+void __print(double x) { cerr << x; }
+void __print(long double x) { cerr << x; }
+void __print(char x) { cerr << '\'' << x << '\''; }
+void __print(const char *x) { cerr << '\"' << x << '\"'; }
+void __print(const string &x) { cerr << '\"' << x << '\"'; }
+void __print(bool x) { cerr << (x ? "true" : "false"); }
+template <typename A>
+void __print(const A &x);
+template <typename A, typename B>
+void __print(const pair<A, B> &p);
+template <typename... A>
+void __print(const tuple<A...> &t);
+template <typename T>
+void __print(stack<T> s);
+template <typename T>
+void __print(queue<T> q);
+template <typename T, typename... U>
+void __print(priority_queue<T, U...> q);
+template <typename A>
+void __print(const A &x) {
+    bool first = true;
+    cerr << '{';
+    for (const auto &i : x) {
+        cerr << (first ? "" : ","), __print(i);
+        first = false;
+    }
+    cerr << '}';
+}
+template <typename A, typename B>
+void __print(const pair<A, B> &p) {
+    cerr << '(';
+    __print(p.first);
+    cerr << ',';
+    __print(p.second);
+    cerr << ')';
+}
+template <typename... A>
+void __print(const tuple<A...> &t) {
+    bool first = true;
+    cerr << '(';
+    apply([&first](const auto &...args) { ((cerr << (first ? "" : ","), __print(args), first = false), ...); }, t);
+    cerr << ')';
+}
+template <typename T>
+void __print(stack<T> s) {
+    vector<T> debugVector;
+    while (!s.empty()) {
+        T t = s.top();
+        debugVector.push_back(t);
+        s.pop();
+    }
+    reverse(debugVector.begin(), debugVector.end());
+    __print(debugVector);
+}
+template <typename T>
+void __print(queue<T> q) {
+    vector<T> debugVector;
+    while (!q.empty()) {
+        T t = q.front();
+        debugVector.push_back(t);
+        q.pop();
+    }
+    __print(debugVector);
+}
+template <typename T, typename... U>
+void __print(priority_queue<T, U...> q) {
+    vector<T> debugVector;
+    while (!q.empty()) {
+        T t = q.top();
+        debugVector.push_back(t);
+        q.pop();
+    }
+    __print(debugVector);
+}
+void _print() { cerr << "]\n"; }
+template <typename Head, typename... Tail>
+void _print(const Head &H, const Tail &...T) {
+    __print(H);
+    if (sizeof...(T))
+        cerr << ", ";
+    _print(T...);
+}
+#ifndef ONLINE_JUDGE
+#define debug(...) cerr << "Line:" << __LINE__ << " [" << #__VA_ARGS__ << "] = ["; _print(__VA_ARGS__)
+#else
+#define debug(...)
+#endif
+/***************************************************************************************************************************************************/
+
+
+
+int nax = 1e7 + 1;
+vector<int> least(nax + 5, 0);
+ 
+void run() {
+    for(int i = 2; i * i <= nax; i++) {
+        if(least[i] == 0) {
+            for(int j = i * i; j < nax; j += i) {
+                if(least[j] == 0) least[j] = i;
+            }
+        }
+    }
+}
+
+
+void solve() {
+    int n;
+    cin >> n;
+    vector<int> arr(n);
+    for(int i = 0; i < n; i++) cin >> arr[i];
+    for(int i = 0; i < n; i++) {
+        int current = arr[i];
+        cout << least[current] << " ";
+    }
+    cout << endl;
+}
+
+
+int main(){
+ 
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif
+ 
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    
+    //int t = 1;
+    run();
+    int t; cin >> t;
+    for(int i = 1; i <= t; i++){
+        //cout << "Case #" << i << ":"<< " ";
+        solve();
+    }
+    return 0;
+}
