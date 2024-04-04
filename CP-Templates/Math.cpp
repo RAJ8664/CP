5b81f2df0241888fe1bbcc22659c53e5823db447
UNKNOWN <rajr86642@gmail.com>
2024-04-04 21:49:16 +0530

    Math.cpp

--- CP-Templates/Math.cpp
+++ CP-Templates/Math.cpp
@@ -41,6 +41,24 @@ struct Math {
 
     Math(){}
 
+    //count number from 1 to n such that gcd(that number , n) is 1;
+    long long phi(long long n) {
+        long long res = 1;
+        for(int i = 2; i * i <= n; i++) {
+            if(n % i == 0) {
+                while(n % i == 0) {
+                     n = n / i;
+                }
+                res = res - res / i;
+            }
+        }
+        if(n > 1) {
+            res = res - res / n;
+        }
+        return res;
+    }
+
+
     // Greatest common divisors between two numbers
 
     ll GCD(ll a, ll b){
